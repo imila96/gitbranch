1e871c42867a658786dec1d8a6533ac2780b7a4e
imila96 <imbsamarakoon1996@gmail.com>
2020-09-18 06:14:52 +0530

    commit newly phys1

---
+++ CscExamples.java
@@ -0,0 +1,29 @@
+public class cscExamples{
+
+int questionNo;
+string  catagory;
+String text;
+
+Public void setQuestionNo(int questionNo){
+	this.questionNo=questionNo;
+}
+public int getQuestionNo(){
+	return questionNo;
+}
+public void setCatagory(String catagory){
+     this.catagory=catagory;
+} 
+public String getCatagory()
+{
+return catagory;	
+}
+public void setText(String text){
+this.text=text;
+	
+}
+public string getText(){
+	return text;
+}
+ 
+
+}
